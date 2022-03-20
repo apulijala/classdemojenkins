cef0f090c86f3fca923890aa62d20a7b277daaee
arvind <apulijala>
2022-03-20 13:26:23 +0000

    adder file

---
+++ adder.py
@@ -0,0 +1,30 @@
+import sys
+import unittest
+
+
+def adder(a: int, b: int) -> int: 
+  return int(a) + int(b)
+
+def main(args): 
+  print(adder(args[1], args[2]))
+
+class TestAdder(unittest.TestCase):
+  def test_adder_success(self):
+    self.assertEqual(adder(3,5), 8)
+
+  def test_adder_typeerror(self):
+    self.assertRaises(TypeError, adder, 5)
+
+  def test_adder_valueoferror(self):
+    self.assertRaises(TypeError, adder, 5)
+
+  def test_main_indexerror(self):
+    self.assertRaises(IndexError, main, [])
+
+
+if __name__ == '__main__':
+  main(sys.argv)
+    
+
+
+
